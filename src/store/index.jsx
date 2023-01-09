2000637722214f1214293945d0a1b9009baa4cb5
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-01-09 17:11:17 -0300

    feat: increase value with a specific number

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -11,11 +11,17 @@ const counterReducer = (
       counter: state.counter + 1,
     };
   }
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+    };
+  }
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
     };
   }
+
   return state;
 };
 
